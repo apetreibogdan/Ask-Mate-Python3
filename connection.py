ca30f77ba5063fc245ee37c7e1c7f2e72ab08af5
apetreibogdan <70704260+apetreibogdan@users.noreply.github.com>
2022-03-14 06:12:43 +0200

    Add functions for voting questions and answers

--- connection.py
+++ connection.py
@@ -1,11 +1,11 @@
 import csv
 import os
-import time
 
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 DATA_HEADER_ANSWER = ['id', 'submission_time', 'vote_number', 'question_id','message', 'image']
+
 
 def read_answers():
     data_list = []
@@ -30,11 +30,7 @@ def write_answers(data_list):
                              'image': answer['image']
                              })
 
-
-
-
-# This function reads the CSV file
-
+            
 def get_data():
     data = []
     with open('./sample_data/question.csv', 'r') as file:
@@ -43,12 +39,19 @@ def get_data():
             data.append(items)
     return data
 
+  
 def write_data(new_data, fieldnames):
     with open('./sample_data/question.csv', 'w') as file:
         csv_write = csv.DictWriter(file, fieldnames=fieldnames)
         csv_write.writeheader()
         for line in new_data:
-            csv_write.writerow({'id': line['id'], 'submission_time': line['submission_time'], 'view_number': line['view_number'],
-                                 'vote_number': line['vote_number'], 'title': line['title'], 'message': line['message'], 'image': line['image']})
+            csv_write.writerow({'id': line['id'],
+                                'submission_time': line['submission_time'],
+                                'view_number': line['view_number'],
+                                'vote_number': line['vote_number'],
+                                'title': line['title'],
+                                'message': line['message'],
+                                'image': line['image']
+                                })
 
 
